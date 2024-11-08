f07ca5c9a4242b6e0f488d959f5749c61f2deedc
John Doe <andreasbolstad@outlook.com>
2024-11-08 14:21:37 +0100

    dwd

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
 function pulls() {
     const primos = Number(prompt("Hvor mange primogems har du?"));
     const intertwinedFate = Number(prompt("Hvor mange Intertwined Fates har du?"));
+    const starglitter = Number(prompt("Hvor mye starglitter har du?"));
+
 
     // Beregn antall pulls
-    const totalPulls = Math.floor(primos / 160) + intertwinedFate;
+    const totalPulls = Math.floor(primos / 160) + (intertwinedFate) + Math.floor(starglitter/5);
 
-        // Log resultatet i konsollen
         document.getElementById("resultat").innerHTML = `Du kan gjøre totalt ${totalPulls} pulls.`;
 }
